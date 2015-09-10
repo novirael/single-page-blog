bd1f16ab5c4ae18583e09cdda6dca4d10dae3ba6
novirael <novirael@gmail.com>
2015-09-10 19:35:16 +0200

    Working hello world with React

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from rest_framework import routers
 
 from content import views
@@ -11,7 +12,7 @@ router.register(r'blog/entries', views.BlogEntryViewSet)
 
 
 urlpatterns = [
-    url(r'^', views.homepage),
+    url(r'^', TemplateView.as_view(template_name='base.html')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include(router.urls)),
--- static/js/components/Layout.jsx
+++ static/js/components/Layout.jsx
@@ -5,5 +5,3 @@ var Layout = React.createClass({
         return <h1>Hello World</h1>
     }
 });
-
-React.render(<Layout/>, document.getElementById("container"));
